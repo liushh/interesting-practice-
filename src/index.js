011cd952b302a41d30ee344047fadf3f6e59332c
Liusha <liusha@wizeline.com>
2017-11-25 10:10:38 -0800

    Introduced redux into the system: added related package to package.json; added the logic to render the app with redux store and reducer; added sample reducer as a placeholder and related test.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+
 import './index.css';
 import App from './App';
+import allReducers from './reducers';
+
+const store = createStore(
+  allReducers,
+  applyMiddleware(thunk)
+);
 
 ReactDOM.render(<App />, document.getElementById('root'));
