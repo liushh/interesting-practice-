788881b02fbafa797697d27c2e6a081f18b1d298
Liusha <liusha@wizeline.com>
2017-11-28 08:02:20 -0600

    added redux concepts to   app.test.js to make sure we can render App component

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { createStore, applyMiddleware } from 'redux';
+
+import allReducers from './reducers';
 import App from './App';
 
+
 it('renders without crashing', () => {
-  // const div = document.createElement('div');
-  // ReactDOM.render(<App />, div);
+  const store = createStore(
+    allReducers,
+    applyMiddleware(thunk)
+  );
+  const div = document.createElement('div');
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    div
+  );
 });
